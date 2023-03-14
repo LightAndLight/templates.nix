a7271b43fd9b495da1eab30956553fcd251a03f2
Isaac Elliott <isaace71295@gmail.com>
2023-03-14 15:43:15 +1000

    bump ghc version

--- haskell/flake.nix
+++ haskell/flake.nix
@@ -6,7 +6,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let 
         pkgs = import nixpkgs { inherit system; };
-        ghcVersion = "924";
+        ghcVersion = "926";
       in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
